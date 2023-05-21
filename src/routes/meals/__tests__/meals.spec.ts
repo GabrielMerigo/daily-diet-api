a0a995bde2bb40427dabc1d193c58fd52e12f129
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-21 12:34:03 -0300

    message

--- src/routes/meals/__tests__/meals.spec.ts
+++ src/routes/meals/__tests__/meals.spec.ts
@@ -62,4 +62,45 @@ describe("Transactions routes", () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual({ meals: [] });
   });
+
+  it("should be able to get a meals by id", async () => {
+    const createUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        image: "/img",
+        name: "Gabriel",
+      })
+      .expect(201);
+
+    const cookie = createUserResponse.get("Set-Cookie");
+
+    const body = {
+      name: "Almoço",
+      description: "Comi uma carne com arroz",
+      date_meal: "12/12/2022",
+      hour_meal: "12:00",
+      is_on_the_diet: true,
+    };
+
+    await request(app.server).post("/meals").set("Cookie", cookie).send(body);
+
+    const allMealsResponse = await request(app.server)
+      .get("/meals")
+      .set("Cookie", cookie);
+
+    const getMealResponse = await request(app.server)
+      .get(`/meals/${allMealsResponse.body.meals[0].id}`)
+      .set("Cookie", cookie)
+      .expect(200);
+
+    expect(getMealResponse.body.meal).toEqual(
+      expect.objectContaining({
+        name: "Almoço",
+        description: "Comi uma carne com arroz",
+        date_meal: "12/12/2022",
+        hour_meal: "12:00",
+        is_on_the_diet: true,
+      })
+    );
+  });
 });
