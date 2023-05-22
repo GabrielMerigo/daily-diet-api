2751f5a5f678245218d97f9a3bb0d8188e8df83a
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-22 06:35:16 -0300

    tests finished!

--- src/routes/metrics/__test__/metrics.spec.ts
+++ src/routes/metrics/__test__/metrics.spec.ts
@@ -3,7 +3,7 @@ import { execSync } from "node:child_process";
 import request from "supertest";
 import { app } from "../../../app";
 
-describe("Meals routes", () => {
+describe("Metrics routes", () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -17,7 +17,7 @@ describe("Meals routes", () => {
     execSync("npm run knex migrate:latest");
   });
 
-  it("should create a new meal", async () => {
+  it("should show metrics for an user", async () => {
     const createUserResponse = await request(app.server)
       .post("/users")
       .send({
@@ -36,11 +36,208 @@ describe("Meals routes", () => {
       is_on_the_diet: true,
     };
 
-    const response = await request(app.server)
-      .post("/meals")
-      .set("Cookie", cookie)
-      .send(body);
+    await request(app.server).post(`/meals`).set("Cookie", cookie).send(body);
 
-    expect(response.statusCode).toEqual(201);
+    const getAllUsersResponse = await request(app.server)
+      .get("/users")
+      .set("Cookie", cookie);
+
+    const response = await request(app.server)
+      .get(`/metrics/${getAllUsersResponse.body.users[0].id}`)
+      .set("Cookie", cookie);
+
+    expect(response.body.metrics).toEqual(
+      expect.objectContaining({
+        meals_total: 1,
+        meals_total_on_the_diet: 1,
+        meals_total_out_the_diet: 0,
+        meals_best_sequence: 1,
+      })
+    );
+  });
+
+  it("should show a best sequence with more than 1", async () => {
+    const createUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        image: "/img",
+        name: "Gabriel",
+      })
+      .expect(201);
+
+    const cookie = createUserResponse.get("Set-Cookie");
+
+    const bodies = [
+      {
+        name: "Almoço",
+        description: "Comi uma carne com arroz",
+        date_meal: "12/12/2022",
+        hour_meal: "12:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Janta",
+        description: "Comi massa",
+        date_meal: "12/12/2022",
+        hour_meal: "23:00",
+        is_on_the_diet: true,
+      },
+    ];
+
+    for await (let body of bodies) {
+      await request(app.server).post(`/meals`).set("Cookie", cookie).send(body);
+    }
+
+    const getAllUsersResponse = await request(app.server)
+      .get("/users")
+      .set("Cookie", cookie);
+
+    const response = await request(app.server)
+      .get(`/metrics/${getAllUsersResponse.body.users[0].id}`)
+      .set("Cookie", cookie);
+
+    expect(response.body.metrics).toEqual(
+      expect.objectContaining({
+        meals_total: 2,
+        meals_total_on_the_diet: 2,
+        meals_total_out_the_diet: 0,
+        meals_best_sequence: 2,
+      })
+    );
+  });
+
+  it("should add a meal out the diet", async () => {
+    const createUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        image: "/img",
+        name: "Gabriel",
+      })
+      .expect(201);
+
+    const cookie = createUserResponse.get("Set-Cookie");
+
+    const bodies = [
+      {
+        name: "Almoço",
+        description: "Comi uma carne com arroz",
+        date_meal: "12/12/2022",
+        hour_meal: "12:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Janta",
+        description: "Comi massa",
+        date_meal: "12/12/2022",
+        hour_meal: "23:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Café",
+        description: "X Burger",
+        date_meal: "13/12/2022",
+        hour_meal: "10:00",
+        is_on_the_diet: false,
+      },
+    ];
+
+    for await (let body of bodies) {
+      await request(app.server).post(`/meals`).set("Cookie", cookie).send(body);
+    }
+
+    const getAllUsersResponse = await request(app.server)
+      .get("/users")
+      .set("Cookie", cookie);
+
+    const response = await request(app.server)
+      .get(`/metrics/${getAllUsersResponse.body.users[0].id}`)
+      .set("Cookie", cookie);
+
+    expect(response.body.metrics).toEqual(
+      expect.objectContaining({
+        meals_total: 3,
+        meals_total_on_the_diet: 2,
+        meals_total_out_the_diet: 1,
+        meals_best_sequence: 2,
+      })
+    );
+  });
+
+  it("should restart the counter when enter a meal out the diet", async () => {
+    const createUserResponse = await request(app.server)
+      .post("/users")
+      .send({
+        image: "/img",
+        name: "Gabriel",
+      })
+      .expect(201);
+
+    const cookie = createUserResponse.get("Set-Cookie");
+
+    const bodies = [
+      {
+        name: "Almoço",
+        description: "Comi uma carne com arroz",
+        date_meal: "12/12/2022",
+        hour_meal: "12:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Janta",
+        description: "Comi massa",
+        date_meal: "12/12/2022",
+        hour_meal: "23:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Café",
+        description: "X Burger",
+        date_meal: "13/12/2022",
+        hour_meal: "10:00",
+        is_on_the_diet: false,
+      },
+      {
+        name: "Lanche da tarde",
+        description: "Pãozinho",
+        date_meal: "13/12/2022",
+        hour_meal: "14:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Janta",
+        description: "Comi uns carbos",
+        date_meal: "13/12/2022",
+        hour_meal: "20:00",
+        is_on_the_diet: true,
+      },
+      {
+        name: "Café",
+        description: "Aveia",
+        date_meal: "15/12/2022",
+        hour_meal: "06:00",
+        is_on_the_diet: true,
+      },
+    ];
+
+    for await (let body of bodies) {
+      await request(app.server).post(`/meals`).set("Cookie", cookie).send(body);
+    }
+
+    const getAllUsersResponse = await request(app.server)
+      .get("/users")
+      .set("Cookie", cookie);
+
+    const response = await request(app.server)
+      .get(`/metrics/${getAllUsersResponse.body.users[0].id}`)
+      .set("Cookie", cookie);
+
+    expect(response.body.metrics).toEqual(
+      expect.objectContaining({
+        meals_total: 6,
+        meals_total_on_the_diet: 5,
+        meals_total_out_the_diet: 1,
+        meals_best_sequence: 3,
+      })
+    );
   });
 });
