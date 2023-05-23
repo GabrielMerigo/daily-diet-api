6d89f7f61a1f718ddaba0645d41437dfa059699d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-23 15:56:10 -0300

    test

--- src/database.ts
+++ src/database.ts
@@ -4,13 +4,8 @@ import { env } from "./env";
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: {
-    host: env.DATABASE_HOST,
-    port: env.DATABASE_PORT,
-    user: env.DATABASE_USER,
-    password: env.DATABASE_PASSWORD,
-    database: env.DATABASE_NAME,
-  },
+  connection:
+    "postgres://dietdb_user:EhQP093vYVTAvhD3oXekzsWxScd7tIFj@dpg-chme83j3cv21t621j6lg-a.oregon-postgres.render.com/dietdb",
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
