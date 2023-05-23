d875d7a79564d21ebcad535653ccbc1b85c8778d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-23 15:58:26 -0300

    adding ssl

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import { env } from "./env";
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection:
-    "postgres://dietdb_user:EhQP093vYVTAvhD3oXekzsWxScd7tIFj@dpg-chme83j3cv21t621j6lg-a.oregon-postgres.render.com/dietdb",
+    "postgres://dietdb_user:EhQP093vYVTAvhD3oXekzsWxScd7tIFj@dpg-chme83j3cv21t621j6lg-a.oregon-postgres.render.com/dietdb?ssl=true",
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
