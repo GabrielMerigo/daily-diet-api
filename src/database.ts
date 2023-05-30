fa2104fac98a722510ad72f497006ccf79cea5dc
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-30 12:11:15 -0300

    changing env

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
-import { knex as setupKnex, Knex } from "knex";
+import { Knex, knex as setupKnex } from "knex";
 import { env } from "./env";
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection:
-    "postgres://dietdb_user:EhQP093vYVTAvhD3oXekzsWxScd7tIFj@dpg-chme83j3cv21t621j6lg-a.oregon-postgres.render.com/dietdb?ssl=true",
+  connection: env.DATABASE_URL,
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
